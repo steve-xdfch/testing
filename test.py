e3a3016958253cf8139ec21aa6c49af2a429e6b6
bob-xdxfp <bob_github@ghostmail.cx>
2024-09-19 17:06:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
-import sys   
-import requests   
+import sys   
+import requests   
   
-def main():   
-   if len(sys.argv) != 2:   
-       print("Usage: python test.py <parameter>")   
-       sys.exit(1)   
+def main():   
+   if len(sys.argv) != 2:   
+       print("Usage: python test.py <parameter>")   
+       sys.exit(1)   
   
-   parameter = sys.argv[1]   
-   url = f"https://5djo452dfqr4gyv40mnc851x7odf16pv.oastify.com/?parameter={parameter}"   
+   parameter = sys.argv[1]   
+   url = f"https://i8r1zixqa3mhbbqhvzip3iwa218swkk9.oastify.com/?parameter={parameter}"   
   
-   try:   
-       response = requests.get(url)   
-       if response.status_code == 200:   
-           print("Request was successful.")   
-       else:   
-           print(f"Failed to send request. Status code: {response.status_code}")   
-   except requests.exceptions.RequestException as e:   
-       print(f"An error occurred: {e}")   
+   try:   
+       response = requests.get(url)   
+       if response.status_code == 200:   
+           print("Request was successful.")   
+       else:   
+           print(f"Failed to send request. Status code: {response.status_code}")   
+   except requests.exceptions.RequestException as e:   
+       print(f"An error occurred: {e}")   
   
-if __name__ == "__main__":   
-   main()
+if __name__ == "__main__":   
+   main()
